f6b2a449d6782ac73ed478ed8d13620fbd1ce7c1
Andre <andre@tos.world>
2024-12-10 16:35:39 -0300

    update: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-    apps : [{
-        name   : "API",
-        script : "pnpm run start"
-    }]
-}
+    apps: [
+        {
+            name: "docs-en",
+            script: "pnpm run start",
+            env: {
+                DOCS_LANG: "en",
+                PORT: 3000 
+            }
+        },
+        {
+            name: "docs-ptbr",
+            script: "pnpm run start",
+            env: {
+                DOCS_LANG: "ptbr",
+                PORT: 3001
+            }
+        }
+    ]
+};
