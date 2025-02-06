c236b048450cdaf940a6b6c2cfbc4af0b982754f
Andre <andre@tos.world>
2025-02-06 05:53:41 -0300

    fix: organize topics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-// Generated automatically by CMMV
+/**                                                                               
+    **********************************************
+    This script was generated automatically by CMMV.
+    It is recommended not to modify this file manually, 
+    as it may be overwritten by the application.
+    **********************************************
+**/
 
 import 'reflect-metadata';
-import { Module, ApplicationTranspile } from '@cmmv/core';
+
+import { Module, ApplicationTranspile, ApplicationConfig } from '@cmmv/core';
+
+//Controllers
+
+//Providers
 
 export let ApplicationModule = new Module('app', {
+    configs: [ApplicationConfig],
     controllers: [],
     providers: [],
     transpilers: [ApplicationTranspile],
