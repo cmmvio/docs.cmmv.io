090da6a6228676d2e2e2bacd8276114d2a6e78fb
Andre <andre@tos.world>
2025-03-25 04:25:48 -0300

    feat: index

--- public/core/1-cmmv.min.cjs
+++ public/core/1-cmmv.min.cjs
@@ -1,6 +1,6 @@
 /*!
  * cmmv.io (c) 2024, Andre Ferreira
- * compiled Tue, 25 Mar 2025 05:55:36 GMT
+ * compiled Tue, 25 Mar 2025 07:12:37 GMT
  * licensed under the MIT license
  * see: https://github.com/cmmvio/cmmv for details
  */
